dd43126727135d182afd9a586969a21d4f29fe7d
Marcin <ozi@post.pl>
2022-09-15 15:56:26 +0200

    flash messages

--- app/Main.js
+++ app/Main.js
@@ -10,17 +10,24 @@ import Home from './components/Home';
 import CreatePost from './components/CreatePost';
 import Axios from 'axios';
 import ViewSinglePost from './components/ViewSinglePost';
+import FlashMessages from './components/FlashMessages';
 Axios.defaults.baseURL = 'http://localhost:8080';
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')));
+  const [flashMessages, setFlashMessages] = useState([]);
+
+  function addFlashMessage(msg) {
+    setFlashMessages((prev) => prev.concat(msg));
+  }
 
   return (
     <BrowserRouter>
+      <FlashMessages messages={flashMessages} />
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
         <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
-        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/create-post" element={<CreatePost addFlashMessage={addFlashMessage} />} />
         <Route path="/post/:id" element={<ViewSinglePost />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
